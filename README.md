23089fbe05bc963077250ce0696146216ec6f150
Neeko Tang <neeko_tvxq@hotmail.com>
2021-01-09 16:02:04 -0500

    add comments to all files and update README.md

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Code-Quiz
+An app will run in rowser, and will feature dynamically updated HTML and CSS powered by JavaScript code.
+
+## Code style
+HTML
+CSS
+JAVA SCRIPT
+
+# Built with
+The user arrives at the landing page and is presented with a call-to-action to "Start Quiz." 
+Also note the navigation option to "View high scores" and the "Timer".
+
+Clicking the "Start Quiz" button presents the user with a multiple-choice questions（total of 5 questions）. 
+The timer is initialized with a value and immediately begins countdown.
+
+Answering incorrectly results in a time penalty (10 seconds are subtracted from time remaining).
+user will gain 25 score if a question was answered correct.
+
+When time runs out and/or all questions are answered, the user is presented with their final score and asked to enter their initials. 
+Their final score and initials are then stored in localStorage. 
+Check "View high scores" will display a user initials and scores list.
+Go back would restart the quiz, clear score will delete all the user's information.
+
+## Screenshots
+![Screenshot1](screenshot/1.png)
+![Screenshot2](screenshot/2.png)
+![Screenshot3](screenshot/3.png)
+![Screenshot4](screenshot/4.png)
+![Screenshot5](screenshot/5.png)
+![Screenshot6](screenshot/6.png)
+
+# Link to Webpage
+[Code-Quiz](https://neeko623.github.io/Code-Quiz/)
--- script.js
+++ script.js
@@ -1,38 +1,44 @@
+// variables for page elements
+// time and score
 var timeremain = 75;
 var penalty = 10;
-
 var scorenumber = document.getElementById("score1");
 var finalscore = document.getElementById("display-score");
 var scorenumber = 0;
 var rightScore = 0;
 
+// sections
+// section intro
 var counter = document.getElementById("countdown");
 var Btn = document.getElementById("Start-Quiz");
 var questionContent = document.getElementById("firstquestion");
 var introduction = document.getElementById("intro-page");
-var checkAnswer = document.getElementById("answer");
 var nameofthequestion = document.getElementById("questions");
-
+var checkAnswer = document.getElementById("answer");
+// answer1
 var answerBtn1 = document.getElementById("answer1");
+// answer2
 var answerBtn2 = document.getElementById("answer2");
+// answer3
 var answerBtn3 = document.getElementById("answer3");
+// answer4
 var answerBtn4 = document.getElementById("answer4");
 
+// goback
 var gobackBtn = document.getElementById("back-btn");
+// submit-scores
 var submitscoreBtn = document.getElementById("save-score");
+// display username
 var usernameinitial = document.getElementById("display-name");
 
+//question progress begin
 var progress = 0;
 var currentQuestion;
 var Title;
 var Option;
 var Answer;
 
-var UserRecord = [];
-
-localStorage.setItem("UserRecord", JSON.stringify(UserRecord));
-
-// questions list
+// Object for question and answer
 var questions = [{
     question: "The condition in an if/else statement is enclosed with ______.",
     options: ["quotes", "curly brackets", "parenthesis", "square brackets"],
@@ -57,17 +63,15 @@ var questions = [{
 ];
 
 //countdown timer setup
-
 var startCountdown = null;
 function countdown(){
     clearInterval(startCountdown);
     startCountdown = window.setInterval(function(){
         timeremain--;
         counter.textContent = timeremain;
-        console.log("time: "+timeremain);
-        console.log("progress"+progress);
         if (parseInt(timeremain) === 0 || progress === questions.length) {
             clearInterval(startCountdown);
+            getEndQuiz();
         }
     },1000);
 };
@@ -87,8 +91,7 @@ function getQuestion() {
     countdown();
 }
 
-// question progress with correct/wrong answer
-
+// question progress with correct/wrong answer, time penalty
 function getQuestionJudge(i){
     progress += 1;
     scorenumber = 0;
@@ -102,23 +105,22 @@ function getQuestionJudge(i){
     }
 };
 
-// Start - Choice buttons
-
+// EventListeners
+// Start timer and display first question when click start quiz
 if (Btn) {
     Btn.addEventListener("click", function () {
         introduction.style.display = "none";
         questionContent.style.display = "block";
-        console.log("trigger!")
         getQuestion();
     })
 }
 
 answerBtn1.addEventListener("click", function () {
-     getQuestionJudge(1);
-     if (progress == questions.length) {
-       getEndQuiz();
-       return;
-     }
+    getQuestionJudge(1);
+    if (progress == questions.length) {
+        getEndQuiz();
+        return;
+     } 
      getQuestion();
  })
 
@@ -126,9 +128,9 @@ answerBtn2.addEventListener("click", function () {
     getQuestionJudge(2);
     if (progress == questions.length) {
       getEndQuiz();
-    } else {
-      getQuestion();
+      return;
     }
+    getQuestion();
 })
 
 answerBtn3.addEventListener("click", function () {
@@ -156,59 +158,55 @@ function getEndQuiz(){
     document.getElementById("score1").textContent = " " + rightScore;
 }
 
+// Add to localstorage
+var UserRecord = [];
+localStorage.setItem("UserRecord", JSON.stringify(UserRecord));
+
+// submit score to display user initials and scores list
 submitscoreBtn.addEventListener("click", function() {
-
-    console.log(localStorage.getItem("UserRecord"));
-
     let userInput = JSON.parse(localStorage.getItem("UserRecord"))
-    document.getElementById("checkhighscore").style.display = "block";
-    document.getElementById("firstquestion").style.display = "none";
-    document.getElementById("highscores").style.display = "none";
+        document.getElementById("checkhighscore").style.display = "block";
+        document.getElementById("firstquestion").style.display = "none";
+        document.getElementById("highscores").style.display = "none";
 
     let initial = document.getElementById("display-name").value;
+        userInput.push({"initial": initial, "score": rightScore});
 
-    userInput.push({"initial": initial, "score": rightScore});
-
-    console.log(userInput);
-
-    localStorage.setItem("UserRecord", JSON.stringify(userInput));
-
+// localstorage to setItem
+localStorage.setItem("UserRecord", JSON.stringify(userInput));
     let userList = document.getElementById("usernameandscore");
-
     userList.innerHTML = "";
-
+    
     userInput.forEach(function(item, i) {
-      let textNodeContent = item["initial"] + " " + "-" + " " + item["score"];
-      console.log(i, item);
-      let eachLi = document.createElement("LI");
-      let textNode = document.createTextNode(textNodeContent);
-      eachLi.appendChild(textNode);
+        let textNodeContent = item["initial"] + " " + "-" + " " + item["score"];
+        let eachLi = document.createElement("LI");
+        let textNode = document.createTextNode(textNodeContent);
+    
+    eachLi.appendChild(textNode);
+        let userList = document.getElementById("usernameandscore");
 
-      let userList = document.getElementById("usernameandscore");
-
-      userList.appendChild(eachLi);
+    userList.appendChild(eachLi);
     });
-
 })
+
+// View high score page 
 var showscorepage = document.getElementById('viewscorepage');
-showscorepage.addEventListener("click", function(event){
+    showscorepage.addEventListener("click", function(event){
     event.preventDefault();
     document.getElementById("checkhighscore").style.display = "block";
     introduction.style.display = "none";
-
 });
 
-
-// click handlers for restart and clearing scoreboard
+// Go Back Button
 gobackBtn.addEventListener("click", function(){
     introduction.style.display = "block";
     document.getElementById("checkhighscore").style.display = "none";
     progress = 0;
     counter.textContent = "75";
     timeremain = 75;
-    console.log(timeremain)
 });
 
+// Clear the scores
 document.getElementById("clear-score").onclick = function(){
     localStorage.setItem("UserRecord", JSON.stringify([]));
     let userList = document.getElementById("usernameandscore");
